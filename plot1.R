2d630c34f6d80055c672f233331a2ce42c730df1
Kevin Thompson <kevin.david.thompson@gmail.com>
2014-07-08 22:35:24 -0500

    adds code for plot1

--- plot1.R
+++ plot1.R
@@ -2,8 +2,16 @@ electric <- read.table("smaller.txt",sep=";",header=TRUE)
 names(electric)
 electric$Date <- as.Date(electric$Date)
 
+# First time just to see it on my screen
 hist(electric$Global_active_power, col="red", 
      main="Global Active Power", 
      xlab="Global Active Power (kilowatts)",
      ylab="Frequency")
 
+# second time to write it to the file
+png(file="plot1.png")
+hist(electric$Global_active_power, col="red", 
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency")
+dev.off()
